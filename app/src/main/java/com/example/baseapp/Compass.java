362f8a022aa5771b6195707ca12d3c63fe39228f
gmltn205 <74577657+gmltn205@users.noreply.github.com>
2024-05-17 10:34:14 +0900

    테스트

--- app/src/main/java/com/example/baseapp/Compass.java
+++ app/src/main/java/com/example/baseapp/Compass.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Compass extends AppCompatActivity implements CompassSensor.CompassListener {
     private CompassSensor compassSensor;
     private TextView azimuthView;
-    private TextView azimuthViewwwwws;
+    private TextView azimuthViewwwwwss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
